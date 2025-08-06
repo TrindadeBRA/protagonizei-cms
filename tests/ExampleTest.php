09fb68f5728c4c5f6c774324c3fa8a318606b348
Lucas Trindade <trindadebra@gmail.com>
2025-08-06 14:17:42 -0300

    fix

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -4,6 +4,10 @@ use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase {
     public function test_soma() {
-        $this->assertEquals(4, 2 + 2);
+        $this->assertEquals(
+            4, 
+            2 + 2,
+            '4 deve ser igual a 2 + 2'
+        );
     }
 }
