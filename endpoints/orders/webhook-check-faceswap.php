425df3f13b1078a9695799c886fbb437306a250d
Lucas Trindade <trindadebra@gmail.com>
2025-08-10 04:57:49 -0300

    remove log

--- endpoints/orders/webhook-check-faceswap.php
+++ endpoints/orders/webhook-check-faceswap.php
@@ -216,8 +216,6 @@ function trinitykit_handle_check_faceswap_webhook($request) {
     $processed = 0;
     $errors = array();
 
-    error_log("[TrinityKit] XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX " . json_encode($orders));
-
     foreach ($orders as $order) {
         $order_id = $order->ID;
         
